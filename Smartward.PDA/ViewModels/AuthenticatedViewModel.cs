8c148e2d6fa8049518ea0597c8f292662df401a7
NikolajLund <com.e.post@gmail.com>
2014-01-15 11:04:33 +0100

    Renamed NFCViewModel and changed the message type subscription

--- Smartward.PDA/ViewModels/AuthenticatedViewModel.cs
+++ Smartward.PDA/ViewModels/AuthenticatedViewModel.cs
@@ -1,35 +1,35 @@
-﻿using System;
+﻿using SmartWard.Commands;
+using SmartWard.Infrastructure;
+using SmartWard.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using SmartWard.ViewModels;
-using Windows.Storage.Streams;
-using System.Diagnostics;
-using SmartWard.Commands;
 using System.Windows.Input;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using Windows.Networking.Proximity;
-using SmartWard.Infrastructure;
+using Windows.Storage.Streams;
 
 namespace SmartWard.PDA.ViewModels
 {
-    class NfcViewModel : ViewModelBase
+    class AuthenticatedViewModel : ViewModelBase
     {
-        private string _Url; 
-        private bool _NfcDetected; 
+        private string _data; 
+        private bool _NfcDetected;
         private ProximityDevice _proximityDevice; 
         private long _MessageType;
 
-        public NfcViewModel() 
+        public AuthenticatedViewModel() 
         { 
             _proximityDevice = ProximityDevice.GetDefault(); 
             if (_proximityDevice != null) 
             { 
                 _proximityDevice.DeviceArrived += _proximityDevice_DeviceArrived; 
                 _proximityDevice.DeviceDeparted += _proximityDevice_DeviceDeparted; 
-                _MessageType = _proximityDevice.SubscribeForMessage("WindowsUri", MessageReceivedHandler); 
+                _MessageType = _proximityDevice.SubscribeForMessage("Windows", MessageReceivedHandler); 
             }
             InitializeNotifications(WardNode.StartWardNodeAsSystem(WebConfiguration.DefaultWebConfiguration));
         } 
@@ -38,7 +38,7 @@ namespace SmartWard.PDA.ViewModels
         void _proximityDevice_DeviceDeparted(ProximityDevice sender) 
         { 
             NfcDetected = false; 
-            Url = "http://"; 
+            Data = null; 
         } 
  
         void _proximityDevice_DeviceArrived(ProximityDevice sender) 
@@ -56,7 +56,7 @@ namespace SmartWard.PDA.ViewModels
                     reader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf16LE; 
                     string receivedString = reader.ReadString(reader.UnconsumedBufferLength / 2 - 1); 
                     Debug.WriteLine("Received message from NFC: " + receivedString); 
-                    Url = receivedString; 
+                    Data = receivedString; 
                 } 
  
             } 
@@ -74,8 +74,8 @@ namespace SmartWard.PDA.ViewModels
             { 
                 using (var writer = new DataWriter{ UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf16LE } ) 
                 { 
-                    Debug.WriteLine("Writing message to NFC: " + Url); 
-                    writer.WriteString(Url); 
+                    Debug.WriteLine("Writing message to NFC: " + Data); 
+                    writer.WriteString(Data); 
                     long id = _proximityDevice.PublishBinaryMessage("WindowsUri:WriteTag", writer.DetachBuffer()); 
                     _proximityDevice.StopPublishingMessage(id); 
                 } 
@@ -100,14 +100,14 @@ namespace SmartWard.PDA.ViewModels
             } 
         } 
  
-        public string Url 
+        public string Data 
         { 
-            get { return _Url; } 
+            get { return _data; } 
             set 
             { 
-                if (value.Equals(_Url)) return; 
+                if (value.Equals(_data)) return; 
                 OnPropertyChanged(); 
-                _Url = value; 
+                _data = value; 
             } 
         } 
  
