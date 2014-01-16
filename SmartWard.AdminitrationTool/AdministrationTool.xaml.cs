6ea3f7e952001e6e8160a4c6013c3bfd4822fe68
NikolajLund <com.e.post@gmail.com>
2014-01-16 01:49:58 +0100

    AdministationTool Initial Commit. UI and Navigation for menu and overviews.

---
+++ SmartWard.AdminitrationTool/AdministrationTool.xaml.cs
@@ -0,0 +1,35 @@
+﻿using SmartWard.AdministrationTool.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SmartWard.AdministrationTool
+{
+    /// <summary>
+    /// Interaction logic for AdministrationTool.xaml
+    /// </summary>
+    public partial class AdministrationTool : Window
+    {
+        public AdministrationTool()
+        {
+            InitializeComponent();
+
+            ContentFrame.NavigationService.Navigate(new MainMenu());
+        }
+
+        private void click_Close(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
