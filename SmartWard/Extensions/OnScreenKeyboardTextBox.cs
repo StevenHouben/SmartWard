59b184b7bbd1b344d3480d73c26b9800d2abe8e1
NikolajLund <com.e.post@gmail.com>
2014-02-05 17:38:01 +0100

    Experimenting with the on screen keyboard

---
+++ SmartWard/Extensions/OnScreenKeyboardTextBox.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Surface.Presentation.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartWard.Extensions
+{
+    public class OnScreenKeyboardTextBox : SurfaceTextBox
+    {
+        private Process _keyboard;
+        public OnScreenKeyboardTextBox()
+        {
+            GotFocus += LaunchKeyboard;
+            LostFocus += CloseKeyboard;
+        }
+
+        private void LaunchKeyboard(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _keyboard = Process.Start(@"C:\Program Files\Common Files\Microsoft Shared\ink\TabTip.exe");
+            
+        }
+
+        private void CloseKeyboard(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _keyboard.CloseMainWindow();
+            _keyboard.Close();
+        }
+    }
+}
