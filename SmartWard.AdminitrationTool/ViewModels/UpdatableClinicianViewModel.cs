f7a05cb2ba595fc106094434d5b0b275719ce0da
Nikolaj Lund <com.e.post@gmail.com>
2014-02-19 18:41:22 +0100

    Admin tool associate access token via nfc reader.

--- SmartWard.AdminitrationTool/ViewModels/UpdatableClinicianViewModel.cs
+++ SmartWard.AdminitrationTool/ViewModels/UpdatableClinicianViewModel.cs
@@ -1,18 +1,27 @@
-﻿using SmartWard.Commands;
+﻿using SmartWard.AdministrationTool.Views;
+using SmartWard.Commands;
 using SmartWard.Models;
 using SmartWard.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using Windows.Networking.Proximity;
 
 namespace SmartWard.AdministrationTool.ViewModels
 {
     class UpdatableClinicianViewModel : ClinicianViewModelBase
     {
-        public UpdatableClinicianViewModel(Clinician clinician) : base(clinician) { }
+        private ProximityDevice _proximityDevice;
+        private Window _associateTokenDialog;
+        public UpdatableClinicianViewModel(Clinician clinician) : base(clinician) 
+        {
+            _proximityDevice = ProximityDevice.GetDefault();
+        }
 
         public event EventHandler ClinicianUpdated;
 
@@ -38,6 +47,52 @@ namespace SmartWard.AdministrationTool.ViewModels
         {
             if (ClinicianUpdated != null)
                 ClinicianUpdated(Clinician, new EventArgs());
+        }
+
+        private ICommand _associateTokenCommand;
+
+        public ICommand AssociateTokenCommand
+        {
+            get
+            {
+                return _associateTokenCommand ?? (_associateTokenCommand = new RelayCommand(
+                    param => AssociateToken(),
+                    param => _proximityDevice != null
+                    ));
+            }
+        }
+
+        private void AssociateToken()
+        {
+            _associateTokenDialog = new AssociateTokenDialogBox() { DataContext = this };
+            _associateTokenDialog.ShowDialog();
+            _proximityDevice.DeviceArrived += _proximityDevice_DeviceArrived;
+        }
+
+        private void _proximityDevice_DeviceArrived(ProximityDevice sender)
+        {
+            Clinician.NfcId = Regex.Match(sender.DeviceId, @"{.*}").Value;
+            CloseAssociateTokenDialog();
+        }
+
+        private ICommand _cancelAssociateTokenCommand;
+
+        public ICommand CancelAssociateTokenCommand
+        {
+            get
+            {
+                return _cancelAssociateTokenCommand ?? (_cancelAssociateTokenCommand = new RelayCommand(
+                    param => CloseAssociateTokenDialog(),
+                    param => true
+                    ));
+            }
+        }
+
+        private void CloseAssociateTokenDialog()
+        {
+            _proximityDevice.DeviceArrived -= _proximityDevice_DeviceArrived;
+            _associateTokenDialog.Close();
+            _associateTokenDialog = null;
         }
     }
 }
