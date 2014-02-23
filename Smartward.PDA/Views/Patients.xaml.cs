4cb133bb3c1d5c0ea9ee8651d4d41f79e51a907e
Nikolaj Lund <com.e.post@gmail.com>
2014-02-23 15:17:15 +0100

    Corrected errors in previous commits.

--- Smartward.PDA/Views/Patients.xaml.cs
+++ Smartward.PDA/Views/Patients.xaml.cs
@@ -21,43 +21,11 @@ namespace SmartWard.PDA.Views
     {
         public Patients()
         {
-            InitializeComponent(); InitializeMapOverlay();
-        }
-        private void InitializeMapOverlay()
-        {
-            var sysRect = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
-            var rect = new Rect(
-                0,
-                0,
-                sysRect.Width,
-                sysRect.Height);
-            popup.Placement = System.Windows.Controls.Primitives.PlacementMode.Absolute;
-            popup.PlacementRectangle = rect;
-            popup.Width = rect.Width;
-            popup.Height = rect.Height;
-            popup.AllowsTransparency = true;
-            popup.PopupAnimation = System.Windows.Controls.Primitives.PopupAnimation.Fade;
-            popup.MouseDown += popup_Down;
-            popup.TouchDown += popup_Down;
+            InitializeComponent(); 
         }
         void popup_Down(object sender, EventArgs e)
         {
             popup.IsOpen = false;
-        }
-
-        private void btnMap_click(object sender, RoutedEventArgs e)
-        {
-            txtMap.Text = popup != null && (popup.IsOpen = !popup.IsOpen) ? "Close Map" : "Map";
-        }
-
-        private void btnSettings_click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnLogout_click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
